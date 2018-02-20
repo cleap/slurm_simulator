643c4380fa9e5848a998ec6df4fad3c02f6f894f
Marco D'Amico <mdamico@bsc.es>
2018-02-20 19:04:30 +0100

    Fixes in swf2trace

--- contribs/simulator/swf2trace_cirne_precise.c
+++ contribs/simulator/swf2trace_cirne_precise.c
@@ -23,7 +23,7 @@ typedef struct job_trace {
     char username[MAX_USERNAME_LEN];
     long int submit; /* relative or absolute? */
     int  duration;
-    int  wclimit;
+    int  wclimit; /*in minutes!*/
     int  tasks;
     char qosname[MAX_QOSNAME];
     char partition[MAX_QOSNAME];
@@ -43,15 +43,14 @@ typedef struct job_trace {
 int main(int argc, char* argv[])
 {
     int i;
-    int idx = 0, errs = 0;
-    int nrecs = 10000; // Number of records has to be entered here each time. This should be done differently.
-    int counter = 0, job_index = 0;
+    int idx=0, errs=0;
+    int nrecs=5000; // Number of records has to be entered here each time. This should be done differently.
     job_trace_t* job_trace,* job_trace_head,* job_arr,* job_ptr;
     char const* const fileName = argv[1]; /* should check that argc > 1 */
     FILE* file = fopen(fileName, "r"); /* should check the result */
     char line[256];
     char *p;
-
+    int job_index = 0, start_time, first_arrival = 0;
     job_arr = (job_trace_t*)malloc(sizeof(job_trace_t)*nrecs);
     if (!job_arr) {
                 printf("Error.  Unable to allocate memory for all job records.\n");
@@ -64,19 +63,24 @@ int main(int argc, char* argv[])
            presence would allow to handle lines longer that sizeof(line) */
         printf("%s", line);
         p = strtok(line, " \t");
-        i=0;
+	i=0;
         while(p!=NULL){
-	    if(i==0) { job_arr[idx].job_id = ++job_index; printf("Index is: %d", job_index); }   
-            if(i==1) { printf("%s", p); job_arr[idx].submit = 100 + atoi(p); }  // why submit cannot start from 0? 
-            if(i==3) { printf("%s", p); job_arr[idx].duration = atoi(p); }   
-            if(i==7) { printf("%s", p); job_arr[idx].tasks = ceil((float) (atoi(p)/CPUS_PER_NODE)); }
-            //if(i==7) { printf("%s", p); job_arr[idx].tasks = ceil((float) atoi(p)); }
-            if(i==8) { printf("%s", p); job_arr[idx].wclimit = ceil((double)atoi(p) / 60.0f); }   
-            //if(i==11) { printf("%s", p); strcpy(job_arr[idx].username, p); }   
+            if(i==0) { job_arr[idx].job_id = ++job_index; printf("Index is: %d", job_index); }
+            else if(i==4) { printf("Submit time: %s\n", p); if (first_arrival == 0) first_arrival =atoi(p); job_arr[idx].submit = 100 + atoi(p) - first_arrival; }  // why submit cannot start from 0? 
+	    else if(i==5) { printf("Ntasks/nodes: %s\n", p); job_arr[idx].tasks = atoi(p); }
+            //else if(i==6) { printf("Wallclock limit: %s\n", p); job_arr[idx].wclimit = ceil((double)atoi(p) / 60.0f); }   
+            else if(i==7) { printf("Startime: %s\n", p); start_time = atoi(p); }
+            else if(i==8) { printf("End time: %s\n", p);
+		job_arr[idx].duration = atoi(p) - start_time; 
+		printf("Duration: %d\n", job_arr[idx].duration);
+	        job_arr[idx].wclimit = ceil((double)job_arr[idx].duration / 60.0f);
+		printf("Wallclock limit:: %d\n", job_arr[idx].duration);
+	    }
+            else printf(" %s\n", p);
+		//if(i==11) { printf("%s", p); strcpy(job_arr[idx].username, p); }   
             //if(i==12) { printf("%s", p); strcpy(job_arr[idx].account, p); }   
             //if(i==15) { printf("%s", p); strcpy(job_arr[idx].partition, p); }   
-            printf(" %s\n", p);
-            p = strtok(NULL," ");
+            p = strtok(NULL," \t");
             i++; 
         }
         // assuming pure MPI application; for threaded one we will have to do it differently.
@@ -84,7 +88,6 @@ int main(int argc, char* argv[])
         //if(job_arr[idx].tasks < CPUS_PER_NODE) job_arr[idx].tasks_per_node = job_arr[idx].tasks; 
         //else job_arr[idx].tasks_per_node = CPUS_PER_NODE;
 
-        if(job_arr[idx].tasks == 0) { job_arr[idx].tasks=1; }
         //assuming one task per node, and as many threads as CPUs per node
         job_arr[idx].cpus_per_task = CPUS_PER_NODE;
         job_arr[idx].tasks_per_node = 1;
@@ -94,13 +97,8 @@ int main(int argc, char* argv[])
         strcpy(job_arr[idx].partition, "normal");
         strcpy(job_arr[idx].account, "1000");
         //strcpy(job_trace->manifest_filename, "|\0");
-        //job_trace->manifest=NULL;
-        if((job_arr[idx].wclimit * 60)> job_arr[idx].duration ){
-          idx++;
-          counter++;
-        } 
-	else
-	  printf("duration greater than wclimit! %d\n", job_arr[idx].job_id);
+        //job_trace->manifest=NULL; 
+        idx++;
     }
     /* may check feof here to make a difference between eof and io failure -- network
        timeout for instance */
@@ -119,8 +117,7 @@ int main(int argc, char* argv[])
     }
     job_ptr = job_trace_head;
     int j=0;
-    int nrecs_limit = counter;
-    while(j<nrecs_limit){
+    while(j<nrecs){
          written = write(trace_file, &job_arr[j], sizeof(job_trace_t));
               if (written <= 0) {
                         printf("Error! Zero bytes written.\n");
